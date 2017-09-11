57e2ea498020289b516895cc98e77f383d090277
Ilja M. Nolte <i.m.nolte@umcg.nl>
2017-09-11 11:59:26 +0000

    version 1.1

--- R/MetaSubtract.r
+++ R/MetaSubtract.r
@@ -1,4 +1,4 @@
-F_translate_headers <- function(header, standard = c("MARKER", "EFFECTALLELE", "OTHERALLELE", "BETA", "SE", "Z", "P", "LP", "EAF", "N", "NSTUDIES", "QHET", "QHETP", "I2HET", "DIRECTION"), alternative) {
+F_translate_headers <- function(header, standard = c("MARKER", "EFFECTALLELE", "OTHERALLELE", "BETA", "SE", "Z", "P", "LP", "EAF", "N", "NSTUDIES", "QHET", "QHETP", "I2HET", "DIRECTIONS"), alternative) {
   capitalized <- toupper(header)
   for(forI in 1:length(standard)) {
     column_no <- F_find_header(standard[forI], alternative, capitalized)
@@ -36,20 +36,49 @@ GClambda <- function(P) {
   return(lambda)
 }
 
+subtract.directions <- function (meta_cohort) {
+  meta_directions <- unlist(strsplit(meta_cohort$DIRECTIONS,split=""))
+  meta_directions[meta_directions=="-"] <- -1
+  meta_directions[meta_directions=="+"] <-  1
+  meta_directions[meta_directions=="?"] <-  0
+  meta_directions <- matrix(as.integer(meta_directions),nrow=nrow(meta_cohort),byrow=T)
+  cohort_directions <- ifelse(meta_cohort$BETA.y < 0, -1, ifelse(meta_cohort$BETA.y > 0, 1, 0))
+  cohort_directions[is.na(cohort_directions)] <- 0
+  
+  ic<-1
+  maxr<-0
+  for (i in 1:ncol(meta_directions)) {
+    if (cor(cohort_directions,meta_directions[,i]) > maxr) {
+	  maxr<-cor(cohort_directions,meta_directions[,i])
+	  ic <- i
+	}
+  }
+  meta_directions.adj <- meta_directions[, -ic]
+  meta_directions.adj[meta_directions.adj==-1] <- "-"
+  meta_directions.adj[meta_directions.adj==1] <-  "+"
+  meta_directions.adj[meta_directions.adj==0] <-  "?"
+  if (ncol(meta_directions.adj)>1) {
+    for (i in 2:ncol(meta_directions.adj)) {
+      meta_directions.adj[,1] <- paste0(meta_directions.adj[,1], meta_directions.adj[,i])
+	}
+  }
+  return(meta_directions.adj[,1])
+}
+
 meta.min.1 <- function(meta_cohort, metamethod = "FIV", lambda.meta = 1, lambda.cohort = 1, gc_meta = TRUE, calculate_lambda.meta = TRUE, calculate_lambdas.cohort = TRUE) {
   if (!(metamethod %in% c("FIV", "FSSW", "FSZ"))) {
     stop("ERROR: Please specify one of the following meta-methods: FIV, FSSW, or FSZ")
   }
 
-  print(paste(" - Pre-set genomic control lambda of the meta-results = ",format(lambda.meta, digits=3), sep=""), quote=FALSE)
+  print(paste(" - Pre-set genomic control lambda of the imported meta-results = ",format(lambda.meta, digits=4), sep=""), quote=FALSE)
   if (calculate_lambdas.cohort) {
     lambda.cohort <- GClambda(meta_cohort$P.y)
-    print(paste(" - Genomic control lambda calculated from the cohort results = ",format(lambda.cohort, digits=3), sep=""), quote=FALSE)
+    print(paste(" - Genomic control lambda calculated from the cohort results = ",format(lambda.cohort, digits=4), sep=""), quote=FALSE)
   } else {
-    print(paste(" - Pre-set genomic control lambda of the cohort results = ",format(lambda.cohort, digits=3), sep=""), quote=FALSE)
+    print(paste(" - Pre-set genomic control lambda of the cohort results = ",format(lambda.cohort, digits=4), sep=""), quote=FALSE)
   }
 
-    if (toupper(metamethod) == "FIV") { #fixed effect inverse variance
+  if (toupper(metamethod) == "FIV") { #fixed effect inverse variance
     adj <- NULL
     if ("BETA.x" %in% names(meta_cohort) & "SE.x" %in% names(meta_cohort)) {
       if ("BETA.y" %in% names(meta_cohort) & "SE.y" %in% names(meta_cohort)) {
@@ -60,6 +89,7 @@ meta.min.1 <- function(meta_cohort, metamethod = "FIV", lambda.meta = 1, lambda.
         meta_cohort$EAF.adj <- meta_cohort$EAF.x
         meta_cohort$N.adj <- meta_cohort$N.x
         meta_cohort$NSTUDIES.adj <- meta_cohort$NSTUDIES
+		meta_cohort$DIRECTIONS.adj <- subtract.directions(meta_cohort)
         meta_cohort$SE.y <- meta_cohort$SE.y/sqrt(lambda.cohort)
         
         subset <- which(!is.na(meta_cohort$BETA.y) & !is.na(meta_cohort$SE.y))
@@ -72,10 +102,10 @@ meta.min.1 <- function(meta_cohort, metamethod = "FIV", lambda.meta = 1, lambda.
         if (gc_meta) {
           if (calculate_lambda.meta) {
             lambda.meta.adj <- GClambda(meta_cohort$P.adj)
-            print(paste(" - Genomic control lambda calculated from the corrected meta-results = ",format(lambda.meta.adj, digits=3), sep=""), quote=FALSE)
+            print(paste(" - Genomic control lambda calculated from the corrected meta-results = ",format(lambda.meta.adj, digits=4), sep=""), quote=FALSE)
           } else {
             lambda.meta.adj <- lambda.meta
-            print(paste(" - Pre-set genomic control lambda applied to the corrected meta-results = ",format(lambda.meta.adj, digits=3), sep=""), quote=FALSE)
+            print(paste(" - Pre-set genomic control lambda applied to the corrected meta-results = ",format(lambda.meta.adj, digits=4), sep=""), quote=FALSE)
           }
           meta_cohort$P.adj[subset] <- 2*pnorm(-abs(meta_cohort$BETA.adj[subset])/(meta_cohort$SE.adj[subset]*sqrt(lambda.meta.adj)))
         }
@@ -83,7 +113,8 @@ meta.min.1 <- function(meta_cohort, metamethod = "FIV", lambda.meta = 1, lambda.
         meta_cohort$EAF.adj[subset] <- (2*meta_cohort$N.adj[subset]*meta_cohort$EAF.adj[subset]-2*meta_cohort$N.y[subset]*meta_cohort$EAF.y[subset])/(2*meta_cohort$N.adj[subset]-2*meta_cohort$N.y[subset])
         meta_cohort$N.adj[subset] <- meta_cohort$N.adj[subset]-meta_cohort$N.y[subset]
         meta_cohort$NSTUDIES.adj[subset] <- meta_cohort$NSTUDIES.adj[subset]-1
-        adj <- meta_cohort[, c("BETA.adj", "SE.adj", "P.adj", "LP.adj", "EAF.adj", "N.adj", "NSTUDIES.adj")]
+
+        adj <- meta_cohort[, c("BETA.adj", "SE.adj", "P.adj", "LP.adj", "EAF.adj", "N.adj", "NSTUDIES.adj", "DIRECTIONS.adj")]
       } else { 
         stop("ERROR: 'BETA' or 'SE' not found in cohort results")
       }
@@ -101,7 +132,10 @@ meta.min.1 <- function(meta_cohort, metamethod = "FIV", lambda.meta = 1, lambda.
         meta_cohort$EAF.adj <- meta_cohort$EAF.x
         meta_cohort$N.adj <- meta_cohort$N.x
         meta_cohort$NSTUDIES.adj <- meta_cohort$NSTUDIES
+		meta_cohort$DIRECTIONS.adj <- subtract.directions(meta_cohort)
+
         meta_cohort$SE.y <- meta_cohort$SE.y/sqrt(lambda.cohort)
+
         subset <- which(!is.na(meta_cohort$BETA.y) & !is.na(meta_cohort$SE.y))
         
         meta.weight <- meta_cohort$N.adj[subset]
@@ -112,10 +146,10 @@ meta.min.1 <- function(meta_cohort, metamethod = "FIV", lambda.meta = 1, lambda.
         if (gc_meta) {
           if (calculate_lambda.meta) {
             lambda.meta.adj <- GClambda(meta_cohort$P.adj)
-            print(paste(" - Genomic control lambda calculated from the corrected meta-results = ",format(lambda.meta.adj, digits=3), sep=""), quote=FALSE)
+            print(paste(" - Genomic control lambda calculated from the corrected meta-results = ",format(lambda.meta.adj, digits=4), sep=""), quote=FALSE)
           } else {
             lambda.meta.adj <- lambda.meta
-            print(paste(" - Pre-set genomic control lambda applied to the corrected meta-results = ",format(lambda.meta.adj, digits=3), sep=""), quote=FALSE)
+            print(paste(" - Pre-set genomic control lambda applied to the corrected meta-results = ",format(lambda.meta.adj, digits=4), sep=""), quote=FALSE)
           }
           meta_cohort$P.adj[subset] <- 2*pnorm(-abs(meta_cohort$BETA.adj[subset])/(meta_cohort$SE.adj[subset]*sqrt(lambda.meta.adj)))
         }
@@ -123,7 +157,8 @@ meta.min.1 <- function(meta_cohort, metamethod = "FIV", lambda.meta = 1, lambda.
         meta_cohort$EAF.adj[subset] <- (2*meta_cohort$N.adj[subset]*meta_cohort$EAF.adj[subset]-2*meta_cohort$N.y[subset]*meta_cohort$EAF.y[subset])/(2*meta_cohort$N.adj[subset]-2*meta_cohort$N.y[subset])
         meta_cohort$N.adj[subset] <- meta_cohort$N.adj[subset]-meta_cohort$N.y[subset]
         meta_cohort$NSTUDIES.adj[subset] <- meta_cohort$NSTUDIES.adj[subset]-1
-        adj <- meta_cohort[, c("BETA.adj", "SE.adj", "P.adj", "LP.adj", "EAF.adj", "N.adj", "NSTUDIES.adj")]
+
+        adj <- meta_cohort[, c("BETA.adj", "SE.adj", "P.adj", "LP.adj", "EAF.adj", "N.adj", "NSTUDIES.adj", "DIRECTIONS.adj")]
       } else { 
         stop("ERROR: 'BETA' or 'N' not found in cohort results")
       }
@@ -152,6 +187,8 @@ meta.min.1 <- function(meta_cohort, metamethod = "FIV", lambda.meta = 1, lambda.
         meta_cohort$EAF.adj <- meta_cohort$EAF.x
         meta_cohort$N.adj <- meta_cohort$N.x
         meta_cohort$NSTUDIES.adj <- meta_cohort$NSTUDIES
+		meta_cohort$DIRECTIONS.adj <- subtract.directions(meta_cohort)
+
         meta_cohort$Z.y <- meta_cohort$Z.y*sqrt(lambda.cohort)
         meta_cohort$SE.y <- meta_cohort$SE.y/sqrt(lambda.cohort)
         
@@ -162,10 +199,10 @@ meta.min.1 <- function(meta_cohort, metamethod = "FIV", lambda.meta = 1, lambda.
         if (gc_meta) {
           if (calculate_lambda.meta) {
             lambda.meta.adj <- GClambda(meta_cohort$P.adj)
-            print(paste(" - Genomic control lambda calculated from the corrected meta-results = ",format(lambda.meta.adj, digits=3), sep=""), quote=FALSE)
+            print(paste(" - Genomic control lambda calculated from the corrected meta-results = ",format(lambda.meta.adj, digits=4), sep=""), quote=FALSE)
           } else {
             lambda.meta.adj <- lambda.meta
-            print(paste(" - Pre-set genomic control lambda applied to the corrected meta-results = ",format(lambda.meta.adj, digits=3), sep=""), quote=FALSE)
+            print(paste(" - Pre-set genomic control lambda applied to the corrected meta-results = ",format(lambda.meta.adj, digits=4), sep=""), quote=FALSE)
           }
           meta_cohort$P.adj[subset] <- 2*pnorm(-abs(meta_cohort$Z.adj[subset]/sqrt(lambda.meta.adj)))
         }
@@ -173,7 +210,8 @@ meta.min.1 <- function(meta_cohort, metamethod = "FIV", lambda.meta = 1, lambda.
         meta_cohort$EAF.adj[subset] <- (2*meta_cohort$N.adj[subset]*meta_cohort$EAF.adj[subset]-2*meta_cohort$N.y[subset]*meta_cohort$EAF.y[subset])/(2*meta_cohort$N.adj[subset]-2*meta_cohort$N.y[subset])
         meta_cohort$N.adj[subset] <- meta_cohort$N.adj[subset]-meta_cohort$N.y[subset]
         meta_cohort$NSTUDIES.adj[subset] <- meta_cohort$NSTUDIES.adj[subset]-1
-        adj <- meta_cohort[, c("Z.adj", "P.adj", "LP.adj", "EAF.adj", "N.adj", "NSTUDIES.adj")]
+
+        adj <- meta_cohort[, c("Z.adj", "P.adj", "LP.adj", "EAF.adj", "N.adj", "NSTUDIES.adj", "DIRECTIONS.adj")]
       } else { 
         stop("ERROR: 'Z' or 'N' not found in cohort results")
       }
@@ -182,7 +220,8 @@ meta.min.1 <- function(meta_cohort, metamethod = "FIV", lambda.meta = 1, lambda.
     }
   }
   lambda.meta.adj <- GClambda(adj$P.adj)
-  print(paste(" - Genomic control lambda calculated of the final corrected meta-results = ",format(lambda.meta.adj, digits=3), sep=""), quote=FALSE)
+  print(paste(" - Genomic control lambda calculated of the final corrected meta-results = ",format(lambda.meta.adj, digits=4), sep=""), quote=FALSE)  
+  
   return(adj)
 }
 
@@ -258,6 +297,11 @@ meta.subtract <- function(metafile, cohortfiles, metamethod = "FIV", lambda.meta
       meta_cohort[subset, c("EAF.y")] <- 1-meta_cohort[subset, c("EAF.y")]
       if (sum(subset>0)) { meta_cohort[subset, c("EFFECTALLELE.y", "OTHERALLELE.y")] <- cbind(complement(meta_cohort[subset, "OTHERALLELE.y"]), complement(meta_cohort[subset, "EFFECTALLELE.y"])) }
       
+      # added by L.Corbin 7th August 2017
+      # corrects error which was leading to assignment of adjusted betas to wrong SNPs
+      meta_cohort <- meta_cohort[order(meta_cohort$order),]
+      # end of edit
+
       adj <- meta.min.1(meta_cohort, metamethod, lambda.meta, lambdas.cohort[which(cohortfile %in% cohortfiles)], gc_meta, calculate_lambda.meta, calculate_lambdas.cohort)
       s <- " Columns that have been corrected are: "
       for (i in names(adj)) {
